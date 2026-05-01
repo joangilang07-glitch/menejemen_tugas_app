1ae9c521ade61b8b53f6e66bbe6ffa95712c9c49
joan <joangilang07@gmail.com>
2026-05-01 17:13:27 +0700

    feat(auth): tambah loginProses dan logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function login()
     {
-        return view('login');
+        return view('auth.login');
+    }
+
+    public function loginProses(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($data)) {
+            return redirect()->route('dashboard')
+                ->with('success', 'Login berhasil! Selamat datang, ' . Auth::user()->nama . '!');
+        } else {
+            return redirect()->back()
+                ->with('error', 'Email atau Password Salah!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login')
+            ->with('success', 'Logout berhasil!');
     }
 }
