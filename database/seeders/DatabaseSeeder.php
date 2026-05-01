86c837ef3661f172bcfbe4c5c2e7eb94621e3211
joan <joangilang07@gmail.com>
2026-05-01 17:17:01 +0700

    feat(database): tambah seeder data user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash; // ← tambahkan ini
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
+        User::create([
+            'nama' => 'Test User',
             'email' => 'test@example.com',
+            'jabatan' => 'admin',
+            'password' => Hash::make('123456789'),
+            'is_tugas' => false,
         ]);
     }
 }
