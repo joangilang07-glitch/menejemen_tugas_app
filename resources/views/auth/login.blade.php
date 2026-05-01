19a47aa173f2a9f8378355c6bd4f63d8d47535e0
joan <joangilang07@gmail.com>
2026-05-01 17:17:49 +0700

    feat(auth): perbaiki form login dan sweetalert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,93 +2,100 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title>M-Tugas | Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
+    <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="{{ asset('sbadmin2/css/sb-admin-2.min.css') }}" rel="stylesheet">
-
 </head>
 
 <body class="bg-gradient-primary">
 
     <div class="container">
-
-        <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-xl-6 col-lg-7 col-md-9">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
                             <div class="col-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4"><i class="fas fa-tasks me-2"></i> 
-                                            M-Tugas | Login
+                                        <h1 class="h4 text-gray-900 mb-4">
+                                            <i class="fas fa-tasks me-2"></i> M-Tugas | Login
                                         </h1>
                                     </div>
-                                   <form class="user" method="POST" action="{{ route('login') }}">
-    @csrf
 
-    <div class="form-group">
-        <input type="email" class="form-control form-control-user"
-            name="email" placeholder="Masukkan Email..." required>
-    </div>
+                                    <form class="user" method="POST" action="{{ route('login.proses') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="email"
+                                                class="form-control form-control-user"
+                                                name="email"
+                                                placeholder="Masukkan Email..."
+                                                value="{{ old('email') }}"
+                                                required>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password"
+                                                class="form-control form-control-user"
+                                                name="password"
+                                                placeholder="Masukkan Password..."
+                                                required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                    </form>
 
-    <div class="form-group">
-        <input type="password" class="form-control form-control-user"
-            name="password" placeholder="Masukkan Password..." required>
-    </div>
-
-    <button type="submit" class="btn btn-primary btn-user btn-block">
-        Login
-    </button>
-</form>
                                     <hr>
                                     <div class="text-center">
-                                        <small>kembali ke beranda? 
-                                        <a href="{{ route('wellcome') }}"
-                                        >klik disini!</a></small>
-                                </div>
+                                        <small>kembali ke beranda?
+                                            <a href="{{ route('wellcome') }}">klik disini!</a>
+                                        </small>
                                     </div>
-                                    <
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
+<script src="{{ asset('sbadmin2/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('sbadmin2/js/demo/datatables-demo.js') }}"></script>
+<script src="{{ asset('swwealot2/dist/sweetalert2.all.min.js') }}"></script>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('sbadmin2/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('sbadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+@if(session('success'))
+<script>
+    window.onload = function() {
+        Swal.fire({
+            title: "Berhasil!",
+            text: "{{ session('success') }}",
+            icon: "success"
+        });
+    }
+</script>
+@endif
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('sbadmin2/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('sbadmin2/js/sb-admin-2.min.js') }}"></script>
+@if(session('error'))
+<script>
+    window.onload = function() {
+        Swal.fire({
+            title: "Gagal!",
+            text: "{{ session('error') }}",
+            icon: "error"
+        });
+    }
+</script>
+@endif
 
 </body>
-
 </html>
